8eef77594557500e3d5a9478dd28d89c8c28b4d7
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-06-02 10:51:09 +0700

    RegisterHook custom Input

--- src/components/form/RegisterHook.jsx
+++ src/components/form/RegisterHook.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import InputHook from "../input/InputHook";
 
 const RegisterHook = () => {
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm();
+  const onSubmitHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div className="max-w-[300px] mx-auto my-10">
+    <form
+      onSubmit={handleSubmit(onSubmitHandler)}
+      className="max-w-[300px] mx-auto my-10"
+    >
       <div className="flex flex-col gap-3">
         <label htmlFor="username" className="cursor-pointer font-medium">
           Username
         </label>
-        <input
-          id="username"
-          type="text"
+        <InputHook
+          name="username"
           placeholder="Enter your username"
-          className="p-4 border border-gray-100 rounded-lg bg-white outline-none transition-all focus:border-blue-500"
-        />
+          id="username"
+          control={control}
+        ></InputHook>
         <p className="text-red-500 text-sm">Please enter your username</p>
       </div>
-    </div>
+      <button className="w-full p-5 bg-blue-500 text-white rounded-lg mt-5 font-semibold">
+        Submit
+      </button>
+    </form>
   );
 };
 
