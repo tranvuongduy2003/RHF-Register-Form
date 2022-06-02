555606097fa836588a286579a3f747e2e25bb58e
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-06-02 13:14:52 +0700

    RegisterHook custom Radio and custom Checkbox

--- src/components/form/RegisterHook.jsx
+++ src/components/form/RegisterHook.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import CheckboxHook from "../checkbox/CheckboxHook";
 import InputHook from "../input/InputHook";
+import RadioHook from "../radio/RadioHook";
 
 const RegisterHook = () => {
   const {
@@ -15,8 +17,9 @@ const RegisterHook = () => {
     <form
       onSubmit={handleSubmit(onSubmitHandler)}
       className="max-w-[300px] mx-auto my-10"
+      autoComplete="off"
     >
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 mb-5">
         <label htmlFor="username" className="cursor-pointer font-medium">
           Username
         </label>
@@ -24,9 +27,60 @@ const RegisterHook = () => {
           name="username"
           placeholder="Enter your username"
           id="username"
+          type="text"
           control={control}
         ></InputHook>
         <p className="text-red-500 text-sm">Please enter your username</p>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <label htmlFor="email" className="cursor-pointer font-medium">
+          Email address
+        </label>
+        <InputHook
+          name="email"
+          placeholder="Enter your email"
+          id="email"
+          type="email"
+          control={control}
+        ></InputHook>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <label htmlFor="password" className="cursor-pointer font-medium">
+          Password
+        </label>
+        <InputHook
+          name="password"
+          placeholder="Enter your password"
+          id="password"
+          type="password"
+          control={control}
+        ></InputHook>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <label htmlFor="password" className="cursor-pointer font-medium">
+          Gender
+        </label>
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-x-3">
+            <RadioHook control={control} name="gender" value="male"></RadioHook>
+            <span>Male</span>
+          </div>
+          <div className="flex items-center gap-x-3">
+            <RadioHook
+              control={control}
+              name="gender"
+              value="female"
+            ></RadioHook>
+            <span>Female</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 mb-5">
+        <CheckboxHook
+          control={control}
+          text="I accept the terms and conditions"
+          name="term"
+        ></CheckboxHook>
       </div>
       <button className="w-full p-5 bg-blue-500 text-white rounded-lg mt-5 font-semibold">
         Submit
